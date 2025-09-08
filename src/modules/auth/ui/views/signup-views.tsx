0fd5b78c766507ae49b67aebe8c331e2e25f5499
FebrianNugraha <febrian.14nugraha@gmail.com>
2025-09-08 20:14:03 +0700

    feat: update auth ui

--- src/modules/auth/ui/views/signup-views.tsx
+++ src/modules/auth/ui/views/signup-views.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Handbag } from "lucide-react";
+import { Eye, EyeOff, Handbag } from "lucide-react";
 import { useForm } from "react-hook-form";
 import z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -18,8 +18,12 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { useState } from "react";
 
 export const SignUpViews = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
   const form = useForm<z.infer<typeof signUpSchemas>>({
     resolver: zodResolver(signUpSchemas),
     defaultValues: {
@@ -36,15 +40,15 @@ export const SignUpViews = () => {
 
   return (
     <div className="flex flex-col w-full gap-y-6 gap-6">
-      <Card>
+      <Card className="shadow-lg border border-gray-100 rounded-2xl">
         <CardHeader>
-          <div className="flex flex-col gap-y-2">
-            <div className="flex gap-x-2 items-center">
-              <Handbag size={18} />
-              <h1 className="text-sm">Create your account</h1>
+          <div className="flex flex-col gap-y-2 text-center">
+            <div className="flex gap-x-2 items-center justify-center">
+              <Handbag size={20} className="text-primary" />
+              <h1 className="text-lg font-semibold">Create your accont</h1>
             </div>
             <p className="text-xs text-muted-foreground">
-              Create your account to get started
+              Sign up to get started
             </p>
           </div>
         </CardHeader>
@@ -96,14 +100,27 @@ export const SignUpViews = () => {
                   render={({ field }) => (
                     <FormItem>
                       <FormLabel>Password</FormLabel>
-                      <FormControl>
-                        <Input
-                          {...field}
-                          type="password"
-                          placeholder="Enter your password"
-                        />
-                      </FormControl>
-                      <FormMessage />
+                      <div className="relative">
+                        <FormControl>
+                          <Input
+                            {...field}
+                            type={showPassword ? "text" : "password"}
+                            placeholder="Enter your password"
+                          />
+                        </FormControl>
+                        <button
+                          type="button"
+                          className="absolute right-0 inset-y-0 pr-3 items-center text-gray-400 hover:text-gray-600"
+                          onClick={() => setShowPassword(!showPassword)}
+                        >
+                          {showPassword ? (
+                            <Eye size={18} />
+                          ) : (
+                            <EyeOff size={18} />
+                          )}
+                        </button>
+                        <FormMessage />
+                      </div>
                     </FormItem>
                   )}
                 />
@@ -113,13 +130,28 @@ export const SignUpViews = () => {
                   render={({ field }) => (
                     <FormItem>
                       <FormLabel>Confirm Password</FormLabel>
-                      <FormControl>
-                        <Input
-                          {...field}
-                          type="password"
-                          placeholder="Re-enter your password"
-                        />
-                      </FormControl>
+                      <div className="relative">
+                        <FormControl>
+                          <Input
+                            {...field}
+                            type={showConfirmPassword ? "text" : "password"}
+                            placeholder="Confirm your password"
+                          />
+                        </FormControl>
+                        <button
+                          type="button"
+                          className="absolute right-0 inset-y-0 pr-3 items-center text-gray-400 hover:text-gray-600"
+                          onClick={() =>
+                            setShowConfirmPassword(!showConfirmPassword)
+                          }
+                        >
+                          {showConfirmPassword ? (
+                            <Eye size={18} />
+                          ) : (
+                            <EyeOff size={18} />
+                          )}
+                        </button>
+                      </div>
                       <FormMessage />
                     </FormItem>
                   )}
