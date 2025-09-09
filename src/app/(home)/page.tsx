04117fd2192e7913f36c336176feef656bba1f54
FebrianNugraha <febrian.14nugraha@gmail.com>
2025-09-09 14:17:35 +0700

    feat: banner section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,11 @@
+import { BannerSection } from "@/modules/home/ui/sections/banner-section";
+
 const Page = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className="w-full max-w-7xl mx-auto py-4">
+      <BannerSection />
+    </div>
+  );
 };
 
 export default Page;
