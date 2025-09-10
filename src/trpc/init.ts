56fdcc527cf658bb299c66a606d22c358cfe6054
FebrianNugraha <febrian.14nugraha@gmail.com>
2025-09-10 13:31:27 +0700

    feat: trpc setup

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,11 +1,8 @@
-import { initTRPC } from "@trpc/server";
+import { auth } from "@/lib/auth";
+import { initTRPC, TRPCError } from "@trpc/server";
+import { headers } from "next/headers";
 import { cache } from "react";
-export const createTRPCContext = cache(async () => {
-  /**
-   * @see: https://trpc.io/docs/server/context
-   */
-  return { userId: "user_123" };
-});
+export const createTRPCContext = cache(async () => {});
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
 // For instance, the use of a t variable
@@ -20,3 +17,22 @@ const t = initTRPC.create({
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+export const protectedProcedure = t.procedure.use(async ({ ctx, next }) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "You must be logged in to access this resource",
+    });
+  }
+
+  return next({
+    ctx: {
+      ...ctx,
+      session,
+    },
+  });
+});
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,17 +1,4 @@
-import { z } from "zod";
-import { baseProcedure, createTRPCRouter } from "../init";
-export const appRouter = createTRPCRouter({
-  hello: baseProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      })
-    )
-    .query((opts) => {
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
-    }),
-});
+import { createTRPCRouter } from "../init";
+export const appRouter = createTRPCRouter({});
 // export type definition of API
 export type AppRouter = typeof appRouter;
