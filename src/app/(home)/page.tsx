6d69a6772400b48ee09880c1f2719a41884525ff
FebrianNugraha <febrian.14nugraha@gmail.com>
2025-09-11 12:41:48 +0700

    feat: categories router

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,8 @@
 import { BannerSection } from "@/modules/home/ui/sections/banner-section";
 import { HomeView } from "@/modules/home/ui/views/home-view";
+import { prefetch, trpc } from "@/trpc/server";
 
-const Page = () => {
+const Page = async () => {
   return <HomeView />;
 };
 
